10d1324653518eba754cde98dcac2ff815355d2d
Ekatmoz <ekatmoz@gmail.com>
2026-01-27 15:12:25 +0100

    minor adjustment to design

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -17,16 +17,18 @@ const Popular = () => {
                 className='aspect-square w-full rounded-md object-cover group-hover:opacity-75 lg:aspect-auto lg:h-60'
               />
               <div className='mt-4 flex justify-between'>
-                <div>
-                  <h3 className='text-sm text-gray-300'>
+                <div> 
+                  <div className='flex justify-between'>
+                    <h3 className='text-sm text-gray-300'>
                     <a href={product.href}>
                       <span aria-hidden='true' className='absolute inset-0' />
                       {product.name}
                     </a>
                   </h3>
-                  <p className='mt-1 text-sm text-gray-500'>{product.description}</p>
+                  <p className='text-sm font-medium text-gray-300'>{product.price}</p></div>
+                  
+                  <div> <p className='mt-1 text-sm text-gray-500'>{product.description}</p></div>               
                 </div>
-                <p className='text-sm font-medium text-gray-300'>{product.price}</p>
               </div>
             </div>
           ))}
--- src/components/data.js
+++ src/components/data.js
@@ -2,7 +2,7 @@ export const products = [
   {
     id: 1,
     name: "Sushi Box",
-    description: "2 db sake maki, 2 db vegán maki, 2 db Arizona maki, 2 db California maki, 1 db ebi nigiri, 1 db chucka nigiri.",
+    description: "2db sake maki, 2db vegán maki, 2db Arizona maki, 2db California maki, 1db ebi nigiri, 1db chucka nigiri.",
     price: "3990Ft",
     image: "/box.webp",
     imageAlt: 'Sushi Box',
@@ -10,7 +10,7 @@ export const products = [
   {
     id: 2,
     name: "Sushi Box Plusz",
-    description: "4 db sake maki, 4 db vegán maki, 2 db Arizona maki, 2 db California maki, 1 db ebi nigiri, 1 db chucka nigiri.",
+    description: "4db sake maki, 4db vegán maki, 2db Arizona maki, 2db California maki, 1db ebi nigiri, 1db chucka nigiri.",
     price: "3990Ft",
     image: "/boxplus.webp",
     imageAlt: 'Sushi Box Plus',
@@ -18,7 +18,7 @@ export const products = [
   {
     id: 3,
     name: "Sushi Ship",
-    description: "8 db sake maki, 8 db uborkás maki, 4 db Arizona maki, 4 db California maki, 2 db ebi nigiri, 2 db chuka nigiri, forrázott szójabab",
+    description: "8db sake maki, 8db uborkás maki, 4db Arizona maki, 4db California maki, 2db ebi nigiri, 2db chuka nigiri, forrázott szójabab",
     price: "9990Ft",
     image: "/ship.webp",
     imageAlt: 'Sushi Ship',
@@ -26,7 +26,7 @@ export const products = [
   {
     id: 4,
     name: "Családi tál",
-    description: "16 db sake maki, 16 db uborkás maki, 8 db Arizona maki, 8 db California maki, 4 db ebi nigiri, 4 db chuka nigiri, forrázott szójabab.",
+    description: "16db sake maki, 16db uborkás maki, 8db Arizona maki, 8db California maki, 4db ebi nigiri, 4db chuka nigiri, forrázott szójabab.",
     price: "17990Ft",
     image: "/familybox.webp",
     imageAlt: 'Family plate',
@@ -34,7 +34,7 @@ export const products = [
   {
     id: 5,
     name: "Prémium Sushi Ship",
-    description: "8 db sake maki, 4 db California maki, 8 db futomaki, 8 db Philadelphia maki, 1 db chucka nigiri, 1 db tobiko gunkan nigiri, 1 db ikura gunkan nigiri, 2 db unagi nigiri, 1 db sake sashimi, forrázott szójabab.",
+    description: "8db sake maki, 4db California maki, 8db futomaki, 8db Philadelphia maki, 1db chucka nigiri, 1db tobiko gunkan nigiri, 1db ikura gunkan nigiri, 2db unagi nigiri, 1db sake sashimi, forrázott szójabab.",
     price: "21990Ft",
     image: "/premium.webp",
     imageAlt: 'Premium Sushi Ship',
